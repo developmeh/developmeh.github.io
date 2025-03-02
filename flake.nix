04034599adc0c0ce57041cccdbdff0bb6aaee1ae
Paul Scarrone <paul@scarrone.co>
2025-03-02 05:47:28 -0500

    more nonsense

--- flake.nix
+++ flake.nix
@@ -1,22 +1,27 @@
 {
   description = "Developmeh.com Env";
 
-  inputs = {
+inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    deploy.url = "path:deploy";
+    deploy.url = "path:./deploy";
   };
 
-  outputs = { self, nixpkgs, deploys }:
+  outputs = { self, nixpkgs, deploy }:
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
         pkgs = import nixpkgs { inherit system; };
+        deployShell = deploy.outputs.devShells.${system}.default;
       });
     in
     {
-      devShells = forEachSupportedSystem ({ pkgs }: {
+      devShells = forEachSupportedSystem ({ pkgs, deployShell }: {
         default = pkgs.mkShell {
-          packages = with pkgs; [ marksman typos-lsp ] ++ deploy.devShells.default.buildInputs;
+          packages = with pkgs; [ marksman typos-lsp ];
+
+          inputsFrom = [
+            deployShell
+          ];
         };
       });
     };
