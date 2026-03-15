155151302ad4f327abd26fa7e71b3750358c94e0
Paul Scarrone <paul@scarrone.co>
2026-03-15 15:43:10 -0400

    added kwike

--- flake.nix
+++ flake.nix
@@ -4,24 +4,27 @@
 inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/25.11";
     deploy.url = "path:./deploy";
+    kwik-e-mart.url = "sourcehut:~ninjapanzer/kwik-e-mart";
   };
 
-  outputs = { self, nixpkgs, deploy }:
+  outputs = { self, nixpkgs, kwik-e-mart, deploy }:
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
         pkgs = import nixpkgs { inherit system; };
+        kwike = kwik-e-mart.packages.${system}.default;
         deployShell = deploy.outputs.devShells.${system}.default;
       });
     in
     {
-      devShells = forEachSupportedSystem ({ pkgs, deployShell }: {
+      devShells = forEachSupportedSystem ({ pkgs, kwike, deployShell }: {
         default = pkgs.mkShell {
           packages = with pkgs; [
             marksman
             typos-lsp
             dasel  # TOML/JSON/YAML manipulation for discussion sync
             jq     # JSON parsing for GitHub GraphQL API
+            kwike
           ];
 
           inputsFrom = [
